7f8dadabe36e34341a273125832cbf8c968215fc
Rinat Farvazov <stiff_rf@mail.ru>
2020-08-17 16:06:32 +0500

    Initial Commit

---
+++ TestActions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestActions
+//
+//  Created by Rinat Farvazov on 8/17/20.
+//  Copyright © 2020 snts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
